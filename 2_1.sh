22b1e1a3fba36a82d5ff38c765243d1a3e513bb5
JayD1 <jayadevnaram@gmail.com>
2017-10-06 03:08:00 +0530

    1 is done

--- 2_1.sh
+++ 2_1.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
+
 a=$1; b=$2
+
 if [ $a -lt $b ]; then
 low=$a; high=$b
 else
 low=$b; high=$a
 fi
-var1=`df -h | grep "/dev/*" | tr -s " " | cut -d" " -f 5 | cut -d"%" -f 1`
-for i in $var1
+
+df -h | grep "/dev/disk" | tr -s " " | cut -d" " -f 1,5-5 | while read i; 
 do
-if [ $i -lt $low ]; then
-echo OK\, `df -h | grep "$i" | cut -d" " -f 1`\, "$i"\%
-elif [ $i -gt $high ]
-then
-echo CRITICAL\, `df -h | grep "$i" | cut -d" " -f 1`\, "$i"\%
+cap=`echo $i | cut -d" " -f 2 | cut -d"%" -f 1`
+disc=`echo $i | cut -d" " -f 1`
+if [ $cap -lt $low ]; then
+echo OK\, $disc\, $cap\%
+elif [ $i -gt $high ]; then
+echo CRITICAL\, $disc\, $cap\%
 else 
-echo WARNING\, `df -h | grep "$i" | cut -d" " -f 1`\, "$i"\%
+echo WARNING\, $disc\, $cap\%
 fi
 done
