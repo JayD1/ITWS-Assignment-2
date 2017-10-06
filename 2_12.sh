9aaff97cb0f742ff722857269d5d65639f008b92
JayD1 <jayadevnaram@gmail.com>
2017-10-06 15:56:57 +0530

    12 is done

---
+++ 2_12.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+a=37
+b=m
+var=`ping -c $1 google.com`
+echo "$var" | while read j; do
+echo -e "\033[$a$b $date -- $j\033[0m"
+a=`expr $a - 1`
+if [ $a -eq 30 ] ; then
+a=37
+fi
+done
