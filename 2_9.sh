d74ea27c7556c0d854fa5dc78f0f8e92a6ca3fe4
JayD1 <31660846+JayD1@users.noreply.github.com>
2017-10-09 09:33:10 +0000

    final!

--- 2_9.sh
+++ 2_9.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-name=$1
-birthdate=$2
+echo Enter name
+read name
+echo Enter birthdate
+read birthdate
 
 yr=`echo $birthdate | cut -d'-' -f -1`
 date=`date +%F | cut -d'-' -f -1`
 mnth=`echo $birthdate | cut -c 6-10`
 if [ `echo $birthdate | cut -c 6-10` == `date +%F | cut -c 6-10` ]; then
-echo Happy Birthday, $name\. You are $((date-yr)) years old today\!
+echo Happy Birthday, $name\.You are $((date-yr)) years old today\!
 fi
