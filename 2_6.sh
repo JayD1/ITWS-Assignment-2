5e14be8d3b048e192c548a7dbad118fcf38ab4eb
JayD1 <31660846+JayD1@users.noreply.github.com>
2017-10-09 09:21:01 +0000

    final!

--- 2_6.sh
+++ 2_6.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
-salary=$1
+echo Enter the monthly salary
+read salary
 salary=$((salary * 12))
 if [ $salary -lt 300000 ]; then
 echo NO TAX PAYMENT REQUIRED
 else
-echo $((salary * 3 / 10))
+tax=`echo "scale=4; $salary * 3 / 10" | bc`
+echo TAX PAYMENT REQUIRED\:$tax
 fi
