89d89247103c68553039eea31aa422dd3654fb43
JayD1 <31660846+JayD1@users.noreply.github.com>
2017-10-09 09:43:23 +0000

    final!

--- 2_12.sh
+++ 2_12.sh
@@ -3,10 +3,13 @@
 a=37
 b=m
 var=`ping -c $1 google.com`
+
 echo "$var" | while read j; do
-echo -e "\033[$a$b $date -- $j\033[0m"
+echo -e "\033[$a$b $(date) -- $j\033[0m"
 a=`expr $a - 1`
+
 if [ $a -eq 30 ] ; then
 a=37
 fi
+
 done
