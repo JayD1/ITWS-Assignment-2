0ff61f2f7b93755bab08a1f9255aeefbd889fc63
JayD1 <31660846+JayD1@users.noreply.github.com>
2017-10-09 09:46:20 +0000

    final!

--- 2_14.sh
+++ 2_14.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 j=0
+
 for i in $@
 do
 arr[j]=$i
 j=`expr $j + 1`
 done
+
 n=`expr $(echo ${#arr[@]}) - 1`
+
 for ((i=1; i<$((n+1)); i++))
 do
-for ((j=0; j<$i; j++))
-do 
-if [ ${arr[$j]} -gt ${arr[$i]} ]; then
-t=${arr[i]}
-arr[$i]=${arr[$j]}
-arr[$j]=$t
-fi
-done
+
+	for ((j=0; j<$i; j++))
+	do 
+		if [ ${arr[$j]} -gt ${arr[$i]} ]; then
+		t=${arr[i]}
+		arr[$i]=${arr[$j]}
+		arr[$j]=$t
+		fi
+	done
 done
 
 echo ${arr[@]}
